9c96b78690534d990897ce9fa4aa1a1d2f823bb1
Sergey Terentev <sergey.terentev@lamoda.ru>
2024-11-25 01:45:56 +0300

    Add allure report

--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -7,24 +7,33 @@ import org.testng.annotations.BeforeSuite;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.example.teamcity.BaseTest;
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.enums.Endpoint;
 import com.example.teamcity.api.models.User;
 import com.example.teamcity.ui.pages.LoginPage;
 
+import io.qameta.allure.selenide.AllureSelenide;
+
+
 
 public class BaseUiTest extends BaseTest {
-private String url;
-
     @BeforeSuite(alwaysRun = true)
     public void setupUiTest() {
         Configuration.browser = Config.getProperty("browser");
-        url = Configuration.baseUrl = "http://" + Config.getProperty("host");
+        Configuration.baseUrl = "http://" + Config.getProperty("host");
+        // НЕТ ПИШИТЕ UI ТЕСТЫ С ЛОКАЛЬНЫМ БРАУЗЕРОМ
+        // А ПОТОМ ЗАПУСКАЕТЕ НА REMOTE BROWSER
         Configuration.remote = Config.getProperty("remote");
         Configuration.browserSize = Config.getProperty("browserSize");
 
         Configuration.browserCapabilities.setCapability("selenoid:options", Map.of("enableVNC", true, "enableLog", true));
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+                .includeSelenideSteps(true));
     }
 
     @AfterMethod(alwaysRun = true)
